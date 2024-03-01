cb1ff0ea063304dff0c0fbecb4a116ae2f9e34e2
Antonio Trentin <30ntin@gmail.com>
2024-03-01 15:35:44 +0100

    added robots & sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+            lastModified: new Date(),
+            priority: 1
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/artista/tommy-grossi`,
+            lastModified: new Date(),
+            priority: 0.9
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/artista/tobia-baser`,
+            lastModified: new Date(),
+            priority: 0.8
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/piercing`,
+            lastModified: new Date(),
+            priority: 0.7
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/contatti`,
+            lastModified: new Date(),
+            priority: 0.6
+        },
+
+    ]
+}
